c9c289112f21059a3a8f8b378a36ba1f776fce98
R0LEX7 <himanshugola1111@gmail.com>
2022-10-15 06:49:47 -0700

    15 oct final

--- game.js
+++ game.js
@@ -1,88 +1,114 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices);
-
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progress-text');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: "What is your name?",
-    choice1: "Varun Dev",
-    choice2: "Indra Dev",
-    choice3: "Ganesh Yadav",
-    choice4: "Himanshu Gola",
-    answer: 4
-  },
-  {
-    question: "What is your age?",
-    choice1: "22",
-    choice2: "21",
-    choice3: "23",
-    choice4: "20",
-    answer: 2
-  },
-  {
-    question: "What is your birth month?",
-    choice1: "july",
-    choice2: "october",
-    choice3: "august",
-    choice4: "march",
-    answer: 3
-  },
-  {
-    question: "What are you persuing right now?",
-    choice1: "Graduation",
-    choice2: "Schooling",
-    choice3: "Post Graduation",
-    choice4: "Employed",
-    answer: 1
-  }
-];
+let questions = [];
 
-//  constants 
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
 
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
+const MAX_QUESTIONS = 10;
 
-startGame = ()=> {
+startGame = () => {
+    
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
-    if(availableQuestions.length == 0 || questionCounter>= MAX_QUESTIONS){
-        // go to end page
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        //go to the end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
-    choices.forEach(choice => {
+
+    choices.forEach((choice) => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
     });
-    availableQuestions.splice(questionIndex,1);
+
+    availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
-choices.forEach(choice=> {
-    choice.addEventListener( "click", e => {
-        if (!acceptingAnswers) returns;
 
-        acceptingAnswers = false ;
-        const selectedChoice  = e.target;
-        const selectedAnswers = selectedChoice.dataset["number"];
-        console.log(selectedAnswers);
-        getNewQuestion();
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 500);
     });
 });
 
-console.log(questions.length);
-startGame();
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
